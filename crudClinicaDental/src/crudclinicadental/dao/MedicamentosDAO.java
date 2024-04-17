b67cf473084ee8920bf30a527e19ad34160f1f4d
Gustavo Marin <wstov@hotmail.com>
2024-04-16 21:43:50 -0600

    MedicamentoDAO
    Se crea el archivo medicamentos dao para la interacción a la base de datos

--- crudClinicaDental/src/crudclinicadental/dao/MedicamentosDAO.java
+++ crudClinicaDental/src/crudclinicadental/dao/MedicamentosDAO.java
@@ -4,10 +4,75 @@
  */
 package crudclinicadental.dao;
 
+import crudclinicadental.entity.MedicamentosEntity;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JTable;
+
 /**
  *
  * @author Wstov
  */
 public class MedicamentosDAO {
+       
+    private String mensaje="";
     
+    public String agregarMedicamentos(Connection con, MedicamentosEntity medicamentosEntity){
+        PreparedStatement pst = null;
+        String sql = "INSERT INTO MEDICAMENTOS (MEDICAMENTOID, NOMBRE, TIPO, DOSIS, DESCRIPCIÓN,PROVEEDORID) "
+                + "VALUES(MEDICAMENTOS_SEQ.NEXTVAL,?,?,?,?,?)";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setString(1, medicamentosEntity.getNombre());
+            pst.setString(2, medicamentosEntity.getTipo());
+            pst.setString(3, medicamentosEntity.getDosis());
+            pst.setString(4, medicamentosEntity.getDescripcion());
+            pst.setInt(5, medicamentosEntity.getIdproveedor());
+            mensaje = "GUARDADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (Exception e) {
+            mensaje = "NO SE GUARDO CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public String modificarMedicamentos(Connection con, MedicamentosEntity medicamentosEntity) {
+        PreparedStatement pst = null;
+        String sql = "UPDATE MEDICAMENTOS SET NOMBRE = ?, TIPO = ?, DOSIS = ?, DESCRIPCIÓN = ?,PROVEEDORID = ?"
+                + "WHERE MEDICAMENTOID = ?";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setString(1, medicamentosEntity.getNombre());
+            pst.setString(2, medicamentosEntity.getTipo());
+            pst.setString(3, medicamentosEntity.getDosis());
+            pst.setString(4, medicamentosEntity.getDescripcion());
+            pst.setInt(5, medicamentosEntity.getIdproveedor());
+            mensaje = "ACTUALIZADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (Exception e) {
+            mensaje = "NO SE ACTUALIZAR CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public String eliminarMedicamentos(Connection con, int id) {
+        PreparedStatement pst = null;
+        String sql = "DELETE FROM MEDICAMENTOS WHERE MEDICAMENTOID = ?";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, id);
+
+            mensaje = "ELIMINADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (Exception e) {
+            mensaje = "NO SE ELIMINO CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public void listarMedicamentos(Connection CON, JTable tabla) {
+    }
 }
--- crudClinicaDental/src/crudclinicadental/entity/MedicamentosEntity.java
+++ crudClinicaDental/src/crudclinicadental/entity/MedicamentosEntity.java
@@ -14,5 +14,72 @@ public class MedicamentosEntity {
     private String tipo;
     private String dosis;
     private String descripcion;
-    private
+    private int idproveedor;
+
+    public MedicamentosEntity() {
+    }
+
+    public MedicamentosEntity(int idMedicamentos, String nombre, String tipo, String dosis, String descripcion, int idproveedor) {
+        this.idMedicamentos = idMedicamentos;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.dosis = dosis;
+        this.descripcion = descripcion;
+        this.idproveedor = idproveedor;
+    }
+
+    public int getIdMedicamentos() {
+        return idMedicamentos;
+    }
+
+    public void setIdMedicamentos(int idMedicamentos) {
+        this.idMedicamentos = idMedicamentos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdproveedor() {
+        return idproveedor;
+    }
+
+    public void setIdproveedor(int idproveedor) {
+        this.idproveedor = idproveedor;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicamentosEntity{" + "idMedicamentos=" + idMedicamentos + ", nombre=" + nombre + ", tipo=" + tipo + ", dosis=" + dosis + ", descripcion=" + descripcion + ", idproveedor=" + idproveedor + '}';
+    }
+    
+    
 }
