0c0d3f57dc0bd83dc204aeedddc33c82101941fe
Gustavo Marin <wstov@hotmail.com>
2024-04-17 17:44:27 -0600

    ComentariosDAO
    Se crea el archivo ComentariosDAO para la interacción con la base de datos y se hacen ajusten al archivo CitasDAO

--- crudClinicaDental/src/crudclinicadental/dao/CitasDAO.java
+++ crudClinicaDental/src/crudclinicadental/dao/CitasDAO.java
@@ -20,7 +20,7 @@ public class CitasDAO {
     public String agregarCita(Connection con, CitasEntity citasEntity){
         PreparedStatement pst = null;
         String sql = "INSERT INTO REGISTRO_CITAS (ID_CITA, HORA, FECHA, CONSULTORIO, ID_PACIENTE, ID_MEDICO) "
-                + "VALUES(CITAS_SEQ.NEXTVAL,?,?,?,?,?,?)";
+                + "VALUES(CITAS_SEQ.NEXTVAL,?,?,?,?,?)";
         try {
             pst = con.prepareStatement(sql);
             pst.setTimestamp(1, Timestamp.valueOf(citasEntity.getHora()));
@@ -75,19 +75,19 @@ public class CitasDAO {
 
     public void listarMedico(Connection con, JTable tabla) {
         DefaultTableModel model;
-        String [] columnas = {"ID","NOMBRE","APELLIDO","CEDULA","TELEFONO","TURNO","ESPECIALIDAD"};
+        String [] columnas = {"ID","HORA","FECHA","CONSULTORIO","ID PACIENTE","ID MEDICO"};
         model = new DefaultTableModel(null, columnas);
         
         String sql = "SELECT * FROM REGISTRO_CITAS ORDER BY ID_CITA";
         
-        String [] filas = new String[7];
+        String [] filas = new String[6];
         Statement st = null;
         ResultSet rs = null;
         try {
             st = con.createStatement();
             rs = st.executeQuery(sql);
             while (rs.next()) {
-                for (int i = 0; i < 7; i++) {
+                for (int i = 0; i < 6; i++) {
                     filas[i] = rs.getString(i+1);
                 }
                 model.addRow(filas);
--- crudClinicaDental/src/crudclinicadental/dao/ComentariosDAO.java
+++ crudClinicaDental/src/crudclinicadental/dao/ComentariosDAO.java
@@ -4,10 +4,102 @@
  */
 package crudclinicadental.dao;
 
+import crudclinicadental.entity.ComentariosEntity;
+import crudclinicadental.entity.MedicoEntity;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Wstov
  */
 public class ComentariosDAO {
+     
+    private String mensaje="";
     
+    public String agregarComentario(Connection con, ComentariosEntity comen){
+        PreparedStatement pst = null;
+        String sql = "INSERT INTO COMENTARIOS_CITA (ID_COMENTARIO, ID_CITA, ID_PACIENTE, FECHA, COMENTARIO) "
+                + "VALUES(COMENTARIOS_SEQ.NEXTVAL,?,?,?,?)";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, comen.getIdCita());
+            pst.setInt(2, comen.getIdPaciente());
+            pst.setDate(2, Date.valueOf(comen.getFecha()));
+            pst.setString(4, comen.getComentario());
+            mensaje = "GUARDADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (SQLException e) {
+            mensaje = "NO SE GUARDO CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public String modificarComentario(Connection con, ComentariosEntity comen) {
+        PreparedStatement pst = null;
+        String sql = "UPDATE COMENTARIOS_CITA SET ID_CITA = ?, ID_PACIENTE = ?, FECHA = ?, COMENTARIO = ?"
+                + "WHERE ID_COMENTARIO = ?";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, comen.getIdCita());
+            pst.setInt(2, comen.getIdPaciente());
+            pst.setDate(2, Date.valueOf(comen.getFecha()));
+            pst.setString(4, comen.getComentario());
+            mensaje = "ACTUALIZADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (Exception e) {
+            mensaje = "NO SE ACTUALIZAR CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public String eliminarComentario(Connection con, int id) {
+        PreparedStatement pst = null;
+        String sql = "DELETE FROM COMENTARIOS_CITA WHERE ID_COMENTARIO = ?";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, id);
+
+            mensaje = "ELIMINADO CORRECTAMENTE";
+            pst.execute();
+            pst.close();
+        } catch (SQLException  e) {
+            mensaje = "NO SE ELIMINO CORRECTAMENTE \n " + e.getMessage();
+        }
+        return mensaje;
+    }
+
+    public void listarComentario(Connection con, JTable tabla) {
+        DefaultTableModel model;
+        String [] columnas = {"ID","ID CITA","ID PACIENTE","FECHA","COMENTARIO"};
+        model = new DefaultTableModel(null, columnas);
+        
+        String sql = "SELECT * FROM COMENTARIOS_CITA ORDER BY ID_COMENTARIO";
+        
+        String [] filas = new String[5];
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(sql);
+            while (rs.next()) {
+                for (int i = 0; i < 5; i++) {
+                    filas[i] = rs.getString(i+1);
+                }
+                model.addRow(filas);
+            }
+            tabla.setModel(model);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "NO SE PUEDE LISTAR LA TABLA");
+        }
+    }
 }
