35ec0c2e1dbdd03cb1ad8228c07ebf04e49451bb
Gustavo Marin <wstov@hotmail.com>
2024-04-17 11:40:32 -0600

    Interfaz grafica pacientes
    Se crea la interfaz grafica de pacientes

--- crudClinicaDental/src/ComentariosEntity.java
+++ crudClinicaDental/src/ComentariosEntity.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,11 @@
  * @author Wstov
  */
 public class ComentariosEntity {
+    private int idComentario;
+    private int idCita;
+    private int idPaciente;
+    private LocalDate fecha;
+    private String comenterio;
+    
     
 }
