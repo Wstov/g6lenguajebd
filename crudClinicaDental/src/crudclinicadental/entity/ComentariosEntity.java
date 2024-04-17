239c36ef15f59881d4cdd5feb8fe761d7b8b2718
Gustavo Marin <wstov@hotmail.com>
2024-04-17 13:19:12 -0600

    ComentariosEntity
    actualización del archivo

--- crudClinicaDental/src/crudclinicadental/entity/ComentariosEntity.java
+++ crudClinicaDental/src/crudclinicadental/entity/ComentariosEntity.java
@@ -4,10 +4,74 @@
  */
 package crudclinicadental.entity;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Wstov
  */
 public class ComentariosEntity {
+    private int idComentario;
+    private int idCita;
+    private int idPaciente;
+    private LocalDate fecha;
+    private String comentario;
+
+    public ComentariosEntity() {
+    }
+
+    public ComentariosEntity(int idComentario, int idCita, int idPaciente, LocalDate fecha, String comentario) {
+        this.idComentario = idComentario;
+        this.idCita = idCita;
+        this.idPaciente = idPaciente;
+        this.fecha = fecha;
+        this.comentario = comentario;
+    }
+
+    public int getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(int idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    @Override
+    public String toString() {
+        return "ComentariosEntity{" + "idComentario=" + idComentario + ", idCita=" + idCita + ", idPaciente=" + idPaciente + ", fecha=" + fecha + ", comentario=" + comentario + '}';
+    }
+    
     
 }
