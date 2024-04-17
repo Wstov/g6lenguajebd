e62374e20a4be517da70d46e40893e6575d94bd4
Gustavo Marin <wstov@hotmail.com>
2024-04-17 13:04:54 -0600

    ComentariosEntity
    Se crea la entidad de comentaros

--- crudClinicaDental/src/ComentariosEntity.java
+++ crudClinicaDental/src/ComentariosEntity.java
@@ -16,6 +16,63 @@ public class ComentariosEntity {
     private int idPaciente;
     private LocalDate fecha;
     private String comenterio;
+
+    public ComentariosEntity() {
+    }
+
+    public ComentariosEntity(int idComentario, int idCita, int idPaciente, LocalDate fecha, String comenterio) {
+        this.idComentario = idComentario;
+        this.idCita = idCita;
+        this.idPaciente = idPaciente;
+        this.fecha = fecha;
+        this.comenterio = comenterio;
+    }
+
+    public int getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(int idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getComenterio() {
+        return comenterio;
+    }
+
+    public void setComenterio(String comenterio) {
+        this.comenterio = comenterio;
+    }
+
+    @Override
+    public String toString() {
+        return "ComentariosEntity{" + "idComentario=" + idComentario + ", idCita=" + idCita + ", idPaciente=" + idPaciente + ", fecha=" + fecha + ", comenterio=" + comenterio + '}';
+    }
+    
     
     
 }
