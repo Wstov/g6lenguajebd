aa62750cbfa08e13f5909786dff442ceaf49a6cb
Gustavo Marin <wstov@hotmail.com>
2024-04-16 21:34:47 -0600

    MedicamentosEntity
    Se crea la entidad para los medicamentos

---
+++ crudClinicaDental/src/crudclinicadental/entity/MedicamentosEntity.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crudclinicadental.entity;
+
+/**
+ *
+ * @author Wstov
+ */
+public class MedicamentosEntity {
+    private int idMedicamentos;
+    private String nombre;
+    private String tipo;
+    private String dosis;
+    private String descripcion;
+    private
+}
